f9229f2f7a40d6cfc9d5f5fca45941ee1f71a525
mandarathavale <mandarathavale29595@gmail.com>
2020-05-02 13:00:07 +0530

    Configuration

--- server/server.js
+++ server/server.js
@@ -1,41 +1,20 @@
-require("dotenv").config();
-const Hapi = require("hapi");
-import { validate } from "./utils/JWTUtils";
+const http = require('http');
+const app = require('./app');
 
-// create a server object
-const server = Hapi.server({
-  port: 3001,
-  host: "localhost"
-});
+/**
+ * Get port from environment and store in Express.
+ */
 
-// register routes
-server.route(require("./routes"));
+const port = process.env.PORT || '3000';
+app.set('port', port);
 
-// server.route({
-//   method: "GET",
-//   path: "/{name}",
-//   // config: { auth:  },
-//   handler: (request, h) => {
-//     return "Hello, " + encodeURIComponent(request.params.name) + "!";
-//   }
-// });
+/**
+ * Create HTTP server.
+ */
 
-const init = async () => {
-  await server.register(require("hapi-auth-jwt2"));
-  server.auth.strategy("jwt", "jwt", {
-    key: process.env.SECRET_KEY, // Never Share your secret key
-    validate: validate, // validate function defined above
-    verifyOptions: { algorithms: ["HS256"] } // pick a strong algorithm
-  });
-  server.auth.default("jwt");
+const server = http.createServer(app);
 
-  await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
-};
-
-process.on("unhandledRejection", err => {
-  console.log(err);
-  process.exit(1);
-});
-
-init();
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port);
